82752b03f20848e25527904f366a3cc90a5af09e
dingshuang <390883541@qq.com>
2020-08-14 10:58:20 +0800

    fetch-third

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <h2>fetch分支</h2>
     <h2>fetch second</h2>
+    <h2>fetch third</h2>
     <h2>num数据绑定：{{ state.num }}</h2>
     <button @click="ageChange">age++</button>
     <hr>
