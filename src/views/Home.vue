6766ae27cb7f370a7ee5a0bb67e5f697dccd8c92
dingshuang <390883541@qq.com>
2020-08-14 11:02:33 +0800

    fetch-four-dds

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <h2>fetch分支</h2>
     <h2>fetch second</h2>
     <h2>fetch third</h2>
+    <h3>fetch four</h3>
     <h2>num数据绑定：{{ state.num }}</h2>
     <button @click="ageChange">age++</button>
     <hr>
